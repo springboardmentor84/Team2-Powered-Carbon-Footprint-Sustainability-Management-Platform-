2a248c778078bfbb036fb4ef8476989560cc99c8
Sirivalli0710​ <sirivalli0710.com>
2026-07-29 14:52:56 +0530

    landing page uploaded

--- frontend/ecotrack-frontend/src/app/features/landing/pages/landing/landing.ts
+++ frontend/ecotrack-frontend/src/app/features/landing/pages/landing/landing.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  standalone: true,
   imports: [],
   templateUrl: './landing.html',
-  styleUrl: './landing.css',
+  styleUrl: './landing.css'
 })
-export class Landing {}
+export class Landing {
+
+  goToLogin() {
+    console.log("Login Clicked");
+  }
+
+  goToSignup() {
+    console.log("Signup Clicked");
+  }
+
+}
